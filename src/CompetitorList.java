a5516a140b98b245c48ca8b3b8513b356794b50c
Dat Nguyen <nguyentiendat2505@pm.me>
2023-12-25 23:41:46 +0000

    Added CompetitorList.java for all the data handling and required logics. + Manager.java for all the Saving and Loading of Data. (0.2/1)

--- src/CompetitorList.java
+++ src/CompetitorList.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
 public class CompetitorList {
+    //
+    private ArrayList<Competitor> competitorList;
+
+    public CompetitorList() {
+        this.competitorList = new ArrayList<Competitor>();
+    }
+
+    public CompetitorList(String filename) {
+        this.competitorList = new ArrayList<Competitor>();
+        this.loadCompetitorList(filename);
+    }
+
+
+
+
+
+
 
 }
