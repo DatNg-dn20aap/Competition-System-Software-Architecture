7f16512665908c13b33da4031b4fb111dcf16bf7
Dat Nguyen <nguyentiendat2505@pm.me>
2023-12-27 18:17:31 +0000

    Testing Changing Page After Button Selection UI.

--- src/UI.java
+++ src/UI.java
@@ -1,16 +1,41 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
+import javax.swing.*;
+import java.awt.*;
 import javax.swing.SwingUtilities;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class UI extends JFrame {
+    private CardLayout cardLayout; // Declare CardLayout
+    private JPanel cardPanel;
 
     public UI() {
         setTitle("My First JFrame");
         setSize(720, 540); // Width, Height
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Add a button to the frame
-        JButton button = new JButton("Click Me");
-        add(button); // This adds the button to the frame
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        // Main panel with a button
+        JPanel mainPanel = new JPanel();
+        JButton button = new JButton("Show White Page");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "WhitePage");
+            }
+        });
+        mainPanel.add(button);
+
+        // White page panel
+        JPanel whitePanel = new JPanel();
+        whitePanel.setBackground(Color.WHITE);
+
+        // Add panels to cardPanel
+        cardPanel.add(mainPanel, "MainPage");
+        cardPanel.add(whitePanel, "WhitePage");
+
+        // Add cardPanel to the frame
+        add(cardPanel);
     }
 
     public static void main(String[] args) {
@@ -21,6 +46,5 @@ public class UI extends JFrame {
             }
         });
     }
-
 
 }
